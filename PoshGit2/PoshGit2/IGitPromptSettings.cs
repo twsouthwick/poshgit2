ac9a1f27eee540cee9525f1519388d5f99c36cf8
Taylor Southwick <twsouthwick@outlook.com>
2015-01-30 22:24:32 -0800

    Add IGitPromptSettings and PSObject implementation

---
+++ PoshGit2/PoshGit2/IGitPromptSettings.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PoshGit2
+{
+    public interface IGitPromptSettings
+    {
+        ConsoleColor BranchAheadForegroundColor { get; }
+        ConsoleColor AfterBackgroundColor { get; }
+        ConsoleColor DelimForegroundColor { get; }
+        string DescribeStyle { get; }
+        ConsoleColor DelimBackgroundColor { get; }
+        ConsoleColor UntrackedBackgroundColor { get; }
+        ConsoleColor BeforeForegroundColor { get; }
+        ConsoleColor BranchAheadBackgroundColor { get; }
+        string UntrackedText { get; }
+        ConsoleColor BranchBackgroundColor { get; }
+        ConsoleColor BeforeIndexForegroundColor { get; }
+        string EnablePromptStatus { get; }
+        bool Debug { get; }
+        bool EnableFileStatus { get; }
+        ConsoleColor DefaultForegroundColor { get; }
+        string AfterText { get; }
+        ConsoleColor BranchBehindForegroundColor { get; }
+        IEnumerable<string> RepositoriesInWhichToDisableFileStatus { get; }
+        ConsoleColor BranchForegroundColor { get; }
+        ConsoleColor BranchBehindAndAheadBackgroundColor { get; }
+        ConsoleColor BranchBehindAndAheadForegroundColor { get; }
+        string BeforeText { get; }
+        ConsoleColor WorkingBackgroundColor { get; }
+        ConsoleColor WorkingForegroundColor { get; }
+        string EnableWindowTitle { get; }
+        ConsoleColor BranchBehindBackgroundColor { get; }
+        string BeforeIndexText { get; }
+        bool ShowStatusWhenZero { get; }
+        string DelimText { get; }
+        ConsoleColor BeforeBackgroundColor { get; }
+        ConsoleColor UntrackedForegroundColor { get; }
+        ConsoleColor IndexForegroundColor { get; }
+        bool AutoRefreshIndex { get; }
+        ConsoleColor IndexBackgroundColor { get; }
+        ConsoleColor AfterForegroundColor { get; }
+        ConsoleColor BeforeIndexBackgroundColor { get; }
+    }
+}
---
+++ PoshGit2/PoshGit2/PSObjectGitPromptSettings.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Runtime.CompilerServices;
+
+namespace PoshGit2
+{
+    public class PSObjectGitPromptSettings : IGitPromptSettings
+    {
+        private readonly PSObject _psobject;
+
+        public PSObjectGitPromptSettings(PSObject psobject)
+        {
+            _psobject = psobject;
+        }
+
+        public ConsoleColor AfterBackgroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        public ConsoleColor AfterForegroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        public string AfterText
+        {
+            get
+            {
+                return ConvertTo<string>();
+            }
+        }
+
+        public bool AutoRefreshIndex
+        {
+            get
+            {
+                return ConvertTo<bool>();
+            }
+        }
+
+        public ConsoleColor BeforeBackgroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        public ConsoleColor BeforeForegroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        public ConsoleColor BeforeIndexBackgroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        public ConsoleColor BeforeIndexForegroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        public string BeforeIndexText
+        {
+            get
+            {
+                return ConvertTo<string>();
+            }
+        }
+
+        public string BeforeText
+        {
+            get
+            {
+                return ConvertTo<string>();
+            }
+        }
+
+        public ConsoleColor BranchAheadBackgroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        public ConsoleColor BranchAheadForegroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        public ConsoleColor BranchBackgroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        public ConsoleColor BranchBehindAndAheadBackgroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        public ConsoleColor BranchBehindAndAheadForegroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        public ConsoleColor BranchBehindBackgroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        public ConsoleColor BranchBehindForegroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        public ConsoleColor BranchForegroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        public bool Debug
+        {
+            get
+            {
+                return ConvertTo<bool>();
+            }
+        }
+
+        public ConsoleColor DefaultForegroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        public ConsoleColor DelimBackgroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        public ConsoleColor DelimForegroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        public string DelimText
+        {
+            get
+            {
+                return ConvertTo<string>();
+            }
+        }
+
+        public string DescribeStyle
+        {
+            get
+            {
+                return ConvertTo<string>();
+            }
+        }
+
+        public bool EnableFileStatus
+        {
+            get
+            {
+                return ConvertTo<bool>();
+            }
+        }
+
+        public string EnablePromptStatus
+        {
+            get
+            {
+                return ConvertTo<string>();
+            }
+        }
+
+        public string EnableWindowTitle
+        {
+            get
+            {
+                return ConvertTo<string>();
+            }
+        }
+
+        public ConsoleColor IndexBackgroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        public ConsoleColor IndexForegroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        public IEnumerable<string> RepositoriesInWhichToDisableFileStatus
+        {
+            get
+            {
+                return ConvertTo<string[]>() ?? Enumerable.Empty<string>();
+            }
+        }
+
+
+        public bool ShowStatusWhenZero
+        {
+            get
+            {
+                return ConvertTo<bool>();
+            }
+        }
+
+        public ConsoleColor UntrackedBackgroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        public ConsoleColor UntrackedForegroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        public string UntrackedText
+        {
+            get
+            {
+                return ConvertTo<string>();
+            }
+        }
+
+        public ConsoleColor WorkingBackgroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        public ConsoleColor WorkingForegroundColor
+        {
+            get
+            {
+                return ConvertTo<ConsoleColor>();
+            }
+        }
+
+        private T ConvertTo<T>([CallerMemberName] string propertyName = null)
+        {
+            var property = _psobject.Properties[propertyName];
+
+            if (property == null)
+            {
+                return default(T);
+            }
+
+            return (T)property.Value;
+        }
+    }
+}
