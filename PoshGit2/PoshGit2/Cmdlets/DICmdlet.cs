3fd2e23f8993194f0d92c7ab1fb42ee082784421
Taylor Southwick <twsouthwick@outlook.com>
2015-01-31 14:15:53 -0800

    Use a lifetime scope for resolution
    This allows us to use SessionState objects

--- PoshGit2/PoshGit2/Cmdlets/DICmdlet.cs
+++ PoshGit2/PoshGit2/Cmdlets/DICmdlet.cs
@@ -1,15 +1,48 @@
 ﻿using Autofac;
+using System;
 using System.Management.Automation;
 
 namespace PoshGit2.Cmdlets
 {
-    public class DICmdlet : Cmdlet
+    public class DICmdlet : PSCmdlet, IDisposable
     {
+        private readonly ILifetimeScope _lifetimeScope;
+
+        public DICmdlet()
+        {
+            _lifetimeScope = PoshGit2Container.Instance.BeginLifetimeScope(builder =>
+           {
+               builder.Register<SessionState>(_ => SessionState).AsSelf();
+           });
+        }
+
         protected override void BeginProcessing()
         {
             base.BeginProcessing();
 
-            PoshGit2Container.Instance.InjectUnsetProperties(this);
+            // TODO: This needs to be here for now, otherwise SessionState is not defined yet.
+            // TODO: Is this called on each time something is piped through?
+            _lifetimeScope.InjectUnsetProperties(this);
+        }
+
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _lifetimeScope.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
         }
     }
 }
