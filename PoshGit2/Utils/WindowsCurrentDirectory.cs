2e0ac8a9f13b8fc745c8ddd6237f5ec7f35252de
Taylor Southwick <twsouthwick@outlook.com>
2015-02-11 19:21:10 -0800

    Add WindowsCurrentDirectory

---
+++ PoshGit2/Utils/WindowsCurrentDirectory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PoshGit2.Utils
+{
+    public class WindowsCurrentDirectory : ICurrentWorkingDirectory
+    {
+        public string CWD { get { return Environment.CurrentDirectory; } }
+
+        public bool IsValid { get { return true; } }
+    }
+}
