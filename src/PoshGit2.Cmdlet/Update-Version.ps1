65951f5e7dac3e137a360b6d0afda105606ead54
Taylor Southwick <twsouthwick@outlook.com>
2015-06-24 17:51:55 -0700

    Fix Update-Version to update version in output directory

--- src/PoshGit2.Cmdlet/Update-Version.ps1
+++ src/PoshGit2.Cmdlet/Update-Version.ps1
@@ -1,4 +1,7 @@
-param([string]$outDir)
+param(
+    [Parameter(Mandatory=$TRUE)]
+    [string]$outDir
+    )
 
 function Update-NuspecVersion (
      [Parameter(Mandatory=$TRUE)]
@@ -6,25 +9,25 @@ function Update-NuspecVersion (
      [Parameter(Mandatory=$TRUE)]
      [String] $Version
 ){
-	if ((Test-Path -Path $FilePath -PathType Leaf) -ne $TRUE) {
-		Write-Error -Message ($FilePath + ' not found.') -Category InvalidArgument;
-		exit 1;
-	}
+    if ((Test-Path -Path $FilePath -PathType Leaf) -ne $TRUE) {
+        Write-Error -Message ($FilePath + ' not found.') -Category InvalidArgument;
+        exit 1;
+    }
 
-	#normalize path
-	$FilePath = (Resolve-Path -Path $FilePath).Path;
+    #normalize path
+    $FilePath = (Resolve-Path -Path $FilePath).Path;
 
-	$nuspecConfig = [xml] (Get-Content -Path $FilePath);
-	$nuspecConfig.DocumentElement.metadata.version = $Version;
+    $nuspecConfig = [xml] (Get-Content -Path $FilePath);
+    $nuspecConfig.DocumentElement.metadata.version = $Version;
 
-	if (!$?) {
-		Write-Error -Message "Unable to perform update.";
-		exit 1;
-	}
-	
-	$nuspecConfig.Save($FilePath);
-	
-	Write-Host "Updated $FilePath"
+    if (!$?) {
+        Write-Error -Message "Unable to perform update.";
+        exit 1;
+    }
+
+    $nuspecConfig.Save($FilePath);
+
+    Write-Host "Updated $FilePath"
 }
 
 function Update-ModuleManifest (
@@ -33,25 +36,25 @@ function Update-ModuleManifest (
      [Parameter(Mandatory=$TRUE)]
      [String] $Version
 ){
-	if ((Test-Path  -Path $FilePath -PathType Leaf) -ne $TRUE) {
-		Write-Error -Message ($FilePath + ' not found.') -Category InvalidArgument;
-		exit 1;
-	}
+    if ((Test-Path  -Path $FilePath -PathType Leaf) -ne $TRUE) {
+        Write-Error -Message ($FilePath + ' not found.') -Category InvalidArgument;
+        exit 1;
+    }
 
-	#normalize path
-	$FilePath = (Resolve-Path -Path $FilePath).Path;
+    # Normalize path
+    $FilePath = (Resolve-Path -Path $FilePath).Path;
 
-	$moduleVersionPattern = "ModuleVersion = '.*'";
-	$newVersion = "ModuleVersion = '" + $Version + "'";
+    $moduleVersionPattern = "ModuleVersion = '.*'";
+    $newVersion = "ModuleVersion = '" + $Version + "'";
 
-	(Get-Content -Path $FilePath) | ForEach-Object {$_ -replace $moduleVersionPattern, $newVersion} | Set-Content -Path $FilePath;
-	
-	Write-Host "Updated $FilePath"
+    (Get-Content -Path $FilePath) | ForEach-Object {$_ -replace $moduleVersionPattern, $newVersion} | Set-Content -Path $FilePath;
+
+    Write-Host "Updated $FilePath"
 }
 
 $version = (Get-ChildItem -Path $outDir\PoshGit2.Cmdlet.dll).VersionInfo.FileVersion
-	
+
 Write-Host "Updating file version: $version"
 
-Update-ModuleManifest $PSScriptRoot\posh-git2.psd1 $version
-Update-NuspecVersion $PSScriptRoot\posh-git2.nuspec $version
+Update-ModuleManifest $outDir\posh-git2.psd1 $version
+Update-NuspecVersion $outDir\posh-git2.nuspec $version
